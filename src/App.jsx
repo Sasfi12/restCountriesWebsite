cd23ca41897f70e0d6422bf675f05a821397d0ce
Sasfi12 <sufyan10.muhammad1070@hotmail.com>
2025-03-13 14:43:12 +0100

    might have accidentally defeated vite.config.js... oops

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,19 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  useEffect()
+  const [data , setData] = useState(null);
+  const [loading , setLoading] = useState(false);
+  const [error , setError] = useState(false)
+  // useEffect(() => {
+  //   fetch("https://restcountries.com/v3.1/all").
+  //   then((response) => response.jons()).
+  //   then((response) => console.log(response))
+  //   .catch((error) => error)
+  // }, [])
+
   return (
     <>
+    <h2>HEELO</h2>
     </>
   )
 }
